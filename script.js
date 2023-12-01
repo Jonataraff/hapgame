180c100b680e3275afedd5ee7c9597dfefd0797c
Jonataraff <jonatarafael209@gmail.com>
2023-12-01 16:31:57 -0300

    Feat: JavaScrip and Footer
    adicionei o java script o footer e algumas melhorias na nav bar

--- script.js
+++ script.js
@@ -1,2 +1,57 @@
+function scrollToSection(sectionId) {
+    var sectionElement = document.getElementById(sectionId);
 
+    if (sectionElement) {
+      
+      sectionElement.scrollIntoView({
+        behavior: 'smooth'
+      });
+    }
+  }
 
+  function validarFormulario() {
+    
+    var valorCampo = document.getElementById('campoInput').value;
+
+    
+    if (valorCampo.trim() === '') {
+      
+      document.getElementById('mensagemErro').innerHTML = 'Por favor, preencha este campo.';
+      
+      
+      return false;
+    } else {
+      
+      document.getElementById('mensagemErro').innerHTML = '';
+      
+      return true;
+    }
+  }
+
+  function validarFormulario() {
+    // Obter os valores dos campos
+    var nome = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+
+    // Validar o campo de nome
+    if (nome.trim() === '') {
+      document.getElementById('mensagemErroName').innerHTML = 'Por favor, insira um nome válido.';
+      return false;
+    } else {
+      document.getElementById('mensagemErroName').innerHTML = '';
+    }
+
+    // Validar o campo de email
+    var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+      document.getElementById('mensagemErroEmail').innerHTML = 'Por favor, insira um e-mail válido.';
+      return false;
+    } else {
+      document.getElementById('mensagemErroEmail').innerHTML = '';
+    }
+
+    // Permitir o envio do formulário se todos os campos forem válidos
+    return true;
+  }
+  
+ 
